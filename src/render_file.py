93dd8cb05878c783816c43f5b135ecf81b49e9af
unknown <elaniobros@gmail.com>
2024-08-13 10:24:24 -0300

    test permanaty

--- src/render_file.py
+++ src/render_file.py
@@ -43,43 +43,32 @@ def render_video(id, value):
 
 
 def main():
-    # while True:
-    for x in ['t1.mp4', 't2.mp4']:
-        file = "{}/{}".format(config.DEFAULT_PATH, x)
-        print(file)
-        out = "{}/hls.m3u8".format(config.RENDER_PATH)
-        stream_params = {
-            "-video_source": file,
-            "-streams": [
-                # Stream1: 1920x1080
-                {"-resolution": "1920x1080", "-video_bitrate": "2000k"},
-                # # Stream2: 1280x720
-                # {"-resolution": "1280x720",  "-video_bitrate": "1500k"},
-                # # Stream3: 640x360
-                # {"-resolution": "640x360", "-video_bitrate": "1000k"},
-                # # Stream3: 320x240
-                # {"-resolution": "320x240", "-video_bitrate": "500k"},
+    while True:
+        for x in ['t1.mp4', 't2.mp4']:
+            file = "{}/{}".format(config.DEFAULT_PATH, x)
+            print(file)
+            out = "{}/hls.m3u8".format(config.RENDER_PATH)
+            stream_params = {
+                "-video_source": file,
+                "-streams": [
+                    # Stream1: 1920x1080
+                    {"-resolution": "1920x1080", "-video_bitrate": "2000k"},
+                    # # Stream2: 1280x720
+                    # {"-resolution": "1280x720",  "-video_bitrate": "1500k"},
+                    # # Stream3: 640x360
+                    # {"-resolution": "640x360", "-video_bitrate": "1000k"},
+                    # # Stream3: 320x240
+                    # {"-resolution": "320x240", "-video_bitrate": "500k"},
 
-            ],
-            "-hls_time": 5,
-            "-livestream": True,
-            # "-hls_playlist_type": "vod",
-            "-hls_list_size": 5,
-            # "-hls_delete_threshold": 5,
-            # "-hls_wrap": 10,
-            "-hls_flags": "delete_segments+append_list+omit_endlist",
-        }
+                ],
+                "-livestream": True,
+                "-hls_time": 5,
+                "-hls_list_size": 10,
+                "-hls_flags": "delete_segments+append_list+omit_endlist",
+                
+            }
 
-        streamer = StreamGear(output=out, format="hls", **stream_params)
+            streamer = StreamGear(output=out, format="hls", **stream_params)
 
-        streamer.transcode_source()
-        # playlist = m3u8.load("{}/stream_{}.m3u8".format(config.RENDER_PATH,0))
-        # print(playlist.segments)
+            streamer.transcode_source()
     streamer.terminate()
-
-    # playlist = enumerate(Playlist_Files.select().where(Playlist_Files.render == 0).dicts().order_by(Playlist_Files.time_start.asc()).dicts())
-    # pool = ThreadPool(processes=2)
-    # for id, file in playlist:
-    #     pool.apply_async(render_video,[file['id'], file])
-    # pool.close()
-    # pool.join()
