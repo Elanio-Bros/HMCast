cd63b76719c60f395ac242a1990d60edf06effd7
unknown <elaniobros@gmail.com>
2024-08-13 21:22:04 -0300

    fix class modules utilze threads peformace

--- src/load_stream.py
+++ src/load_stream.py
@@ -1,25 +1,31 @@
 import os
 from . import config
 import shutil
+import threading as thread
 from .Models import Playlist_Files, Catalog_Files
 from datetime import datetime, timedelta
 from vidgear.gears import StreamGear
+import m3u8
+from multiprocessing.pool import ThreadPool
 
 
 def main():
     catalog_id = None
 
     # Tempo de agora
-    date = datetime.now()
-    day_week = date.weekday()
-    time_start = date.strftime("%H:%M:%S")
+    # date = datetime.now()
+    # day_week = date.weekday()
+    # time_start = date.strftime("%H:%M:%S")
 
-    playlist = Playlist_Files.select().where(Playlist_Files.day_week == day_week).where(
-        Playlist_Files.time_start >= time_start).order_by(Playlist_Files.time_start.asc()).dicts()
-    for midia in playlist:
-        if (midia['catalog_id'] == catalog_id or datetime.now().strftime("%H:%M") == midia['time_start'].strftime("%H:%M")):
-            dir = '{}/{}'.format(config.RENDER_PATH, midia['file'])
-            if os.path.isdir(dir) and os.path.exists(dir):
+    # playlist = Playlist_Files.select().where(Playlist_Files.day_week == day_week).where(
+    #     Playlist_Files.time_start >= time_start).order_by(Playlist_Files.time_start.asc()).dicts()
+
+    playlist = [{'file': 't1.mp4'}, {'file': 't2.mp4'}]
+    while True:
+        for midia in playlist:
+            # if (midia['catalog_id'] == catalog_id or datetime.now().strftime("%H:%M") == midia['time_start'].strftime("%H:%M")):
+            dir = '{}/{}'.format(config.TEMP_PATH, midia['file'])
+            if os.path.exists(dir):
                 stream_params = {
                     "-video_source": dir,
                     "-streams": [
@@ -40,12 +46,13 @@ def main():
 
                 }
 
-            streamer = StreamGear(output="{}/hls.m3u8".format(config.DEFAULT_PATH),
-                                  format="hls", custom_ffmpeg=config.IMAGEIO_FFMPEG_EXE, **stream_params)
-            streamer.transcode_source()
-            streamer.terminate()
+                streamer = StreamGear(output="{}/hls.m3u8".format(config.DEFAULT_PATH),
+                                      format="hls", custom_ffmpeg=config.IMAGEIO_FFMPEG_EXE, **stream_params)
+                streamer.transcode_source()
+                streamer.terminate()
 
             # # Removendo para limpeza
+            thread.Thread(target=__stream_unused_files).start()
             # Playlist_Files.delete_by_id(midia['id'])
             # Catalog_Files.update({"watched": 1}).where(
             #     Catalog_Files.id == midia['file_id']).execute()
@@ -53,9 +60,19 @@ def main():
             # catalog_id = midia['catalog_id']
 
 
-# def __removing_unused_files(self):
-    # segments = [segment.uri for segment in original.segments]
-    # for file in os.listdir():
-    #     local_file = "{}/{}".format(self.default_path, file)
-    #     if os.path.isfile(local_file) and os.path.splitext(file)[1] == '.ts' and "chunk-stream{}-".format(self.resolution) in file and file not in segments:
-    #         os.remove(local_file)
+def __stream_unused_files():
+    pool = ThreadPool(processes=2)
+    for resolution in range(0, 2):
+         pool.apply_async(__removing_files,[resolution])
+    pool.close()
+    pool.join()
+
+def __removing_files(resolution):
+    file_playlist = "{}/stream_{}.m3u8".format(config.DEFAULT_PATH, resolution)
+    if (os.path.exists(file_playlist)):
+        playlist = m3u8.load(file_playlist)
+        segments = [segment.uri for segment in playlist.segments]
+        for file in os.listdir(config.DEFAULT_PATH):
+            local_file = "{}/{}".format(config.DEFAULT_PATH, file)
+            if os.path.isfile(local_file) and os.path.splitext(file)[1] == '.ts' and "chunk-stream{}-".format(resolution) in file and file not in segments:
+                os.remove(local_file)
