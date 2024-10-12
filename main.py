190e8222f412040faebe0a606e0c31be47e0bcdb
unknown <elaniobros@gmail.com>
2024-10-12 12:34:44 -0300

    fix files

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import threading as thread
-import time
 import src.include_files as include_files
 import src.load_stream as load_stream
-from datetime import datetime, timedelta
 
 thread.Thread(target=load_stream.main).start()
 thread.Thread(target=include_files.files_minute).start()
